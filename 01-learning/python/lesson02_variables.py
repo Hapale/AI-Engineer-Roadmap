6732eca34feb55d4e7b1dd67a35235c8ce79ffa6
Hapale <160270935+Hapale@users.noreply.github.com>
2026-07-04 11:55:21 +0330

    Learn variables and input

---
+++ 01-learning/python/lesson02_variables.py
@@ -0,0 +1,12 @@
+print("Wellcome to AI Engineer Roadmap")
+
+name = input("What is your name ? ")
+age = input("How old are you ? ")
+city = input("Where are you from ? ")
+job = input("What is your job ? ")
+
+print("Hello",name)
+print("You are",age,"years old")
+print("You are from",city)
+print("Your job is",job)
+print("Welcome to the course!")
