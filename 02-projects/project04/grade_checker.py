d66211fa3f9bdba82d62a8fbd8206939dc2e1a47
Hapale <160270935+Hapale@users.noreply.github.com>
2026-07-05 14:17:18 +0330

    04-05

---
+++ 02-projects/project04/grade_checker.py
@@ -0,0 +1,15 @@
+print("====== Grade Checker ======")
+
+score = int(input("Enter your score: "))
+
+if score >=90:
+    print("Excellent")
+    
+elif score >=80:
+    print("Very good")
+
+elif score >=70:
+    print("Good")
+
+else:
+    print("Need More Practice")
---
+++ 02-projects/project05/hello_loop.py
@@ -0,0 +1,9 @@
+print("===== Hello Loop =====")
+
+for i in range(10):
+    print("Hello")
+
+name = input('Enter your name: ')
+
+for i in range(3):
+    print("Welcome", name)
